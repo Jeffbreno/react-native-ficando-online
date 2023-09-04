1e4497202741b5de454175b78c6ec3c49b5da8ec
Jefferson Lopes <jeffbreno@gmail.com>
2023-09-04 18:03:09 -0300

    04092023_1800

--- src/paginas/Principal/index.js
+++ src/paginas/Principal/index.js
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
-import {
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  TextInput,
-  Alert,
-  ScrollView,
-} from "react-native";
+import { Text, View, Image, TouchableOpacity, TextInput, Alert, ScrollView } from "react-native";
 import estilos from "./estilos";
 import api from "../../services/api";
 import { buscaUsuario } from "../../services/requests/users";
@@ -47,32 +39,20 @@ export default function Principal({ navigation }) {
             <Text style={estilos.textoEmail}>{usuario.email}</Text>
             <View style={estilos.seguidoresArea}>
               <View style={estilos.seguidores}>
-                <Text style={estilos.seguidoresNumero}>
-                  {usuario.followers}
-                </Text>
+                <Text style={estilos.seguidoresNumero}>{usuario.followers}</Text>
                 <Text style={estilos.seguidoresTexto}>Seguidores</Text>
               </View>
               <View style={estilos.seguidores}>
-                <Text style={estilos.seguidoresNumero}>
-                  {usuario.following}
-                </Text>
+                <Text style={estilos.seguidoresNumero}>{usuario.following}</Text>
                 <Text style={estilos.seguidoresTexto}>Seguindo</Text>
               </View>
             </View>
-            <TouchableOpacity
-              onPress={() => navigation.navigate("Repositorios")}
-            >
+            <TouchableOpacity onPress={() => navigation.navigate("Repositorios", { id: usuario.id })}>
               <Text style={estilos.repositorios}>Ver os repositórios</Text>
             </TouchableOpacity>
           </>
         )}
-        <TextInput
-          placeholder="Busque por um usuário"
-          autoCapitalize="none"
-          style={estilos.entrada}
-          value={nomeUsuario}
-          onChangeText={setNomeUsuario}
-        />
+        <TextInput placeholder="Busque por um usuário" autoCapitalize="none" style={estilos.entrada} value={nomeUsuario} onChangeText={setNomeUsuario} />
 
         <TouchableOpacity onPress={busca} style={estilos.botao}>
           <Text style={estilos.textoBotao}>Buscar</Text>
